2c1603cb54e885f1ae16928255037ebcdc375b34
hema1229 <634572723@qq.com>
2015-11-19 16:51:21 +0800

    123

--- src/com/test/CXFTest.java
+++ src/com/test/CXFTest.java
@@ -17,7 +17,7 @@ public class CXFTest {
 //		QueryPortalRolesResp resp = bu.queryPortalRoles(req);
 //		System.out.println(resp.getResultCode());
 //		System.out.println(resp.getRoleList().get(0).getRoleName());
-		//分支之中最终在这种 
+		//分支之中最终在这种 123123
 		StringBuffer s = new StringBuffer("123");
 		test(s);
 		System.out.println(s);
